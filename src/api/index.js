f4ab495fb15f34ab6a7e68251103d46b0ea564eb
Amber Ramesh <amramesh@iu.edu>
2021-10-23 16:30:01 -0400

    Update axios instance configuration - Update param serializer to handle object arrays - Use credentials to send request cookies to server

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,8 @@ import qs from 'qs'
 
 const instance = axios.create({
   baseURL: 'https://iub-wellness-tracker-api.herokuapp.com/api',
-  paramsSerializer: (params) => qs.stringify(params)
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'indices', allowDots: true }),
+  withCredentials: true
 })
 
 export default instance
