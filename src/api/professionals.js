f53288aa444a3ae00d63ca20ea9a28e670420d14
dilzat1027 <dilzat1027@gmail.com>
2021-12-07 10:03:32 -0500

    update chat and profile

--- src/api/professionals.js
+++ src/api/professionals.js
@@ -1,16 +1,26 @@
 import resourse from './index.js'
 
 export default {
+  // Get list of all professionals in database
   getProfessionals () {
     return resourse.get('/getProfessionals')
   },
+  // Get list of all subscribed professionals of current user (id => current user's id)
   getsubbedPros () {
     return resourse.get('/subscription')
   },
+  getSubbedUsers (id) {
+    return resourse.get(`/user/${id}/subscriber/`)
+  },
+  // Get count of suscribed professionals of current user (id => current user's id)
   getSubcount (id) {
     return resourse.get(`/subscription/${id}/count`)
   },
-  addProfessionals (proid) {
-    return resourse.post('subscription/' + proid)
+  getSubUserCount (id) {
+    return resourse.get(`/user/${id}/subscriber/count`)
+  },
+  // Post Request to add the selected professionals to current user subscribed list (id => id of the selected professional)
+  addProfessionals (id) {
+    resourse.post(`/subscription/${id}`)
   }
 }
